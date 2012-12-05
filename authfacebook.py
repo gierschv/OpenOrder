71f795c08aaecb7c20d93a64eb32fdcf2e846c52
Vincent Giersch <vincent@giersch.fr>
2012-12-05 16:38:17 +0000

    /api/auth

--- authfacebook.py
+++ authfacebook.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python
-#temporary files with basics functions 
 
 import facebook
-import os.path
-import wsgiref.handlers
-import logging
-import urllib2
 import entities
-
-
-#o = entities.Order(name="sauter de porc", nbVote=0)
-#        q = entities.Component.all()
-#       	q.filter("name = ", "concombre")
-#        for p in q.run() :
-#	        o.ingredient.append(p.key())
-#		o.put()
+import webapp2
+import string
+import random
+import json
 
 FACEBOOK_APP_ID = "530821850262515"
-FACEBOOK_APP_SECRET = "your app secret"
+FACEBOOK_APP_SECRET = "ba52337c7a1065d5051a132da9dfd831"
 
-from google.appengine.ext import db
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-from google.appengine.ext.webapp import template
-from google.appengine.api.urlfetch import fetch
+class AuthHandler(webapp2.RequestHandler):
+	def get(self):
+		# Check signed request for uid
+		uid = self.request.GET['userID']
+		signedRequest = self.request.GET['signedRequest']
+		if not uid or not signedRequest:
+			return self.abort(404)
 
-class authFacebook(long uid, string ApiKey)
-	user = User.get_by_uid(uid)
+		parsed_request = facebook.parse_signed_request(self.request.GET['signedRequest'], FACEBOOK_APP_SECRET)
+		if not parsed_request or uid != parsed_request['user_id']:
+			return self.abort(403)
+
+		try:
+			result = facebook.get_access_token_from_code(parsed_request["code"], "", FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+			profile = facebook.GraphAPI(result['access_token']).get_object("me")
+		except facebook.GraphAPIError:
+			return self.abort(403)
+
+		# Fetch existant user or create new one
+		user = entities.User.get_by_key_name(profile['id'])
 		if not user:
-			graph = facebook.GraphAPI(cookie["access_token"])
-			profile = graph.get_object("me")
-			user = User(key_name=str(profile["id"]),
-						id=str(profile["id"]),
-						name=profile["name"])
+			api_key = ''. join(random.choice(string.hexdigits) for x in range(32))
+			user = entities.User(key_name = str(profile['id']),
+								uid = int(profile['id']),
+								first_name = profile['first_name'], last_name = profile['last_name'],
+								api_key = api_key, access_token = result['access_token'], active = True)
+		else:
+			user.access_token = result['access_token']
+		user.put();
 
-
+		self.response.out.write(json.dumps(profile))
--- entities.py
+++ entities.py
@@ -4,23 +4,21 @@ from google.appengine.ext import db
 from google.appengine.ext.db import polymodel
 
 class User(db.Model):
+	uid = db.IntegerProperty(required = True)
+	api_key = db.StringProperty(required = True)
+	access_token = db.StringProperty()
 	first_name = db.StringProperty()
 	last_name = db.StringProperty()
-	inscription = db.DateProperty()
 	active = db.BooleanProperty()
-	facebookToken = db.StringProperty(required=True)
-	uid = db.IntegerProperty(required=True)
-	email = db.EmailProperty()
 
 class Component(db.Model):
-	name = db.StringProperty(required=True)
-	stock = db.IntegerProperty(required=True)
-	Step = db.IntegerProperty(required=True)
+	name = db.StringProperty(required = True)
+	stock = db.IntegerProperty(required = True)
+	Step = db.IntegerProperty(required = True)
 
 class favoritOrder(db.Model):
-	name = db.StringProperty(required=True)
+	name = db.StringProperty(required = True)
 	ingredient = db.ListProperty(db.Key)
 	nbVote = db.IntegerProperty()
 
-class CurrentOrder(db.Model)
 
