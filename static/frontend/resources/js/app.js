2a158dd0078b97e1ef70baeda044cf1a9929f43a
Gregoire Liglet <gregoire.liglet@gmail.com>
2012-12-08 21:03:33 +0000

    after merge

--- static/frontend/resources/js/app.js
+++ static/frontend/resources/js/app.js
@@ -10,45 +10,26 @@ $(document).ready(function() {
  
 	var accessToken, profile;
 	var alreadyConnected = 0;
-	var displayFriends = function () {
-		var url_friends =  "https://graph.facebook.com/fql?q=SELECT%20name,%20uid,%20pic_square%20FROM%20user%20WHERE%20has_added_app=1%20and%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20805037276)&access_token="+ accessToken;
-		console.log(url_friends);
-		$.get(url_friends, null, function (data) {
-			friends_obj = $.parseJSON(data);
-			for (i = 0; i < 2; i++) {
-				$("#friends_list").append("<li><a href=''><img id='friend_pic_"+i+"' src='"+friends_obj.data[i].pic_square+"' align='middle'/><h3 id='friend_name_" + i + "'>"+friends_obj.data[i].name+"</h3></a></li>").trigger("create");
-			}
-			$("#friends_list").append("<li><a href='#friendsAll'><h3 id='friends_all'> All my friends </h3></a></li>").trigger("create");
-			$("#friends_list").listview("refresh");
-			for (i = 0; i < friends_obj.data.length; i++) {
-				$("#friends_all_page").append("<li><a href=''><h3 id='friend_name_" + i + "'>"+friends_obj.data[i].name+"</h3><img id='friend_pic_"+i+"' src='"+friends_obj.data[i].pic_square+"' /></a></li>").trigger("create");
-			}
-		});
-	}
-	var displayFamous = function () {
-		$.get(url_friends, null, function (data) {
-			friends_obj = $.parseJSON(data);
-			for (i = 0; i < 2; i++) {
-				$("#friends_list").append("<li><a href=''><img id='friend_pic_"+i+"' src='"+friends_obj.data[i].pic_square+"' align='middle'/><h3 id='friend_name_" + i + "'>"+friends_obj.data[i].name+"</h3></a></li>").trigger("create");
-			}
-			$("#friends_list").append("<li><a href='#friendsAll'><h3 id='friends_all'> All my friends </h3></a></li>").trigger("create");
-			$("#friends_list").listview("refresh");
-			for (i = 0; i < friends_obj.data.length; i++) {
-				$("#friends_all_page").append("<li><a href=''><h3 id='friend_name_" + i + "'>"+friends_obj.data[i].name+"</h3><img id='friend_pic_"+i+"' src='"+friends_obj.data[i].pic_square+"' /></a></li>").trigger("create");
-			}
-		});
-	}
 	
 	var eventLogin = function(response) {
 		if (alreadyConnected == 1) {
 			return;
 		}
 		if (response.status === 'connected') {
-			console.log(response);
 			accessToken = response.authResponse.accessToken;
+			var url_friends =  "https://graph.facebook.com/fql?q=SELECT%20name,%20uid,%20pic_square%20FROM%20user%20WHERE%20has_added_app=1%20and%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20=%20805037276)&access_token="+ accessToken;
 			alreadyConnected = 1;
-			displayFriends();
-			displayFamous();
+			$.get(url_friends, null, function (data) {
+				friends_obj = $.parseJSON(data);
+				for (i = 0; i < 2; i++) {
+					$("#friends_list").append("<li><a href=''><img id='friend_pic_"+i+"' src='"+friends_obj.data[i].pic_square+"' align='middle'/><h3 id='friend_name_" + i + "'>"+friends_obj.data[i].name+"</h3></a></li>").trigger("create");
+				}
+				$("#friends_list").append("<li><a href='#friendsAll'><h3 id='friends_all'> All my friends </h3></a></li>").trigger("create");
+				$("#friends_list").listview("refresh");
+				for (i = 0; i < friends_obj.data.length; i++) {
+					$("#friends_all_page").append("<li><a href=''><h3 id='friend_name_" + i + "'>"+friends_obj.data[i].name+"</h3><img id='friend_pic_"+i+"' src='"+friends_obj.data[i].pic_square+"' /></a></li>").trigger("create");
+				}
+			});
 			if (profile === undefined) {
 				$.get('/api/auth', response.authResponse , function (data) {
 					profile = data;
